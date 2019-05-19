1b0c086293162ad80327fd8a7df45a4348e2e876
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2019-05-19 22:24:10 +0900

    Fix to work in Java 8

--- src/main/kotlin/koma/meta/ObjectMeta.kt
+++ src/main/kotlin/koma/meta/ObjectMeta.kt
@@ -2,15 +2,14 @@ package koma.meta
 
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
+import kotlin.reflect.KVisibility
 import kotlin.reflect.full.memberProperties
-import kotlin.reflect.jvm.javaGetter
 import kotlin.reflect.jvm.jvmErasure
 
 data class ObjectMeta(val kClass: KClass<*>) {
-    val props: Collection<KProperty1<*, *>> = kClass.memberProperties.filter { it.javaGetter != null }
+    val props: Collection<KProperty1<*, *>> = kClass.memberProperties.filter { it.visibility == KVisibility.PUBLIC }
     fun toMap(obj: Any): Map<String, Pair<*, KClass<*>>> {
         return props
-            .filter { it.javaGetter!!.canAccess(obj) }
             .associate { it.name to (it.call(obj) to it.returnType.jvmErasure) }
     }
 }
