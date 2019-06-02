1f119424d5d947a0f71a555946f09a193ceb6be4
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2019-06-02 21:01:15 +0900

    Set up jar publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,18 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import tanvd.kosogor.proxy.publishJar
 
 plugins {
     java
     kotlin("jvm") version "1.3.31"
+    id("tanvd.kosogor") version "1.0.4" apply true
 }
 
 group = "org.komapper"
-version = "0.1-SNAPSHOT"
+version = "0.1"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
@@ -27,6 +30,24 @@ tasks.withType<Test> {
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
+
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+publishJar {
+    publication {
+        artifactId = "komapper"
+    }
+
+    bintray {
+        username = project.properties["bintrayUser"]?.toString() ?: System.getenv("BINTRAY_USER")
+        secretKey = project.properties["bintrayApiKey"]?.toString() ?: System.getenv("BINTRAY_API_KEY")
+        repository = "maven"
+        info {
+            githubRepo = "https://github.com/nakamura-to/komapper.git"
+            vcsUrl = "https://github.com/nakamura-to/komapper.git"
+            license = "Apache-2.0"
+        }
+    }
 }
