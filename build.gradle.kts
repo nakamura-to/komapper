57828f1e6f41c2a86d8b2cb032d3f6cff0360efd
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-02-28 14:10:59 +0900

    Update dependency Kotlin to v1.4.31

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 plugins {
-    kotlin("jvm") version "1.4.0" apply false
+    kotlin("jvm") version "1.4.31" apply false
     id("tanvd.kosogor") version "1.0.4"
     id("org.jlleitschuh.gradle.ktlint") version "8.2.0"
 }
